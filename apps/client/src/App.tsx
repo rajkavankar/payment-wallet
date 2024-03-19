21a45de53f5ed17ae52c9a24519f429a9149e064
Raj kavankar <rajkavankar@gmail.com>
2024-03-19 17:27:03 +0530

    frontend setup with tailwind and shadcn-ui

---
+++ apps/client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@repo/ui/button"
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl'>Hello</h1>
+      <Button className='bg-primary'>Hi there</Button>
+    </div>
+  )
+}
+
+export default App
