291862bad6facb0f5f9c4b77339e2e1f8615ac75
Raj kavankar <rajkavankar@gmail.com>
2024-04-09 21:31:15 +0530

    database added

--- apps/client-app/app/page.tsx
+++ apps/client-app/app/page.tsx
@@ -1,9 +1,15 @@
 import React from "react"
+import { db } from "@repo/database"
 
-const HomePage = () => {
+const HomePage = async () => {
+  const data = await db.user.findMany()
+
+  console.log(data)
+
   return (
     <div>
       <h1 className='text-5xl text-red-300'>Hello</h1>
+      <h1 className='text-5xl'>{JSON.stringify(data)}</h1>
     </div>
   )
 }
---
+++ packages/database/src/db.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+  var db: PrismaClient | undefined
+}
+
+export const db = global.db || new PrismaClient()
+
+if (process.env.NODE_ENV !== "production") global.db = db
+
+export * from "@prisma/client"
